c7b3e730e3291971d95050926d1b79b9feab3a79
chriszarate <chris@zarate.org>
2014-07-25 16:51:13 -0400

    Improve jQuery injection (fixes #2).

--- bookmarkleter.js
+++ bookmarkleter.js
@@ -10,6 +10,9 @@ var specialCharacters = ['%', '"', '<', '>', '#', '@', ' ', '\\&', '\\?'];
 // CDN URL for jQuery.
 var jQueryURL = '//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js';
 
+// Default minimum jQuery version.
+var jQueryMinVersion = 1.7;
+
 // Create a bookmarklet.
 var bookmarkleter = function (code, options) {
 
@@ -21,19 +24,31 @@ var bookmarkleter = function (code, options) {
     mangle: options.mangleVars
   };
 
-  // Add jQuery, if requested.
+  // Add jQuery, if requested (also adds IIFE wrapper).
   if (options.jQuery) {
-    code = 'var __hasjq = function () {' + code + '};' +
-    'if (window.jQuery) __hasjq();' +
-    'var s = document.createElement("script");' +
-    's.src = "' + jQueryURL + '";' +
-    's.onload = __hasjq;' +
-    'document.body.appendChild(s);';
+    code =
+      '(function ($) {' +
+      '  var loadBookmarklet = function ($) {' + code + '};' +
+      '  var hasJQuery = $ && $.fn && parseFloat($.fn.jquery) >= ' + jQueryMinVersion + ';' +
+      '  if(hasJQuery) {' +
+      '    loadBookmarklet($);' +
+      '  } else {' +
+      '    var s = document.createElement("script");' +
+      '    s.src = "' + jQueryURL + '";' +
+      '    s.onload = s.onreadystatechange = function () {' +
+      '      var state = this.readyState;' +
+      '      if(!state || state === "loaded" || state === "complete") {' +
+      '        loadBookmarklet(jQuery.noConflict());' +
+      '      }' +
+      '    };' +
+      '  }' +
+      '  document.getElementsByTagName("head")[0].appendChild(s);' +
+      '})(window.jQuery);';
   }
 
-  // Add anonymous function wrapper, if requested.
-  if (options.anonymize) {
-    code = '(function(){' + code + '})()';
+  // Add IIFE wrapper, if requested.
+  if (options.anonymize && !options.jQuery) {
+    code = '(function () {' + code + '})();';
   }
 
   // Parse and uglify code.
--- test/injectJQuery.js
+++ test/injectJQuery.js
@@ -2,15 +2,15 @@
 
 var bookmarkleter = require('../bookmarkleter');
 
-exports.testInjectJQuery = function (test) {
+exports.injectJQuery = function (test) {
 
   var options = {
     jQuery: true
   };
 
   var data = [
-    ['var test;', 'javascript:var%20__hasjq=function(){};window.jQuery%26%26__hasjq();var%20s=document.createElement(%22script%22);s.src=%22//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js%22,s.onload=__hasjq,document.body.appendChild(s);'],
-    ['console.log("test");', 'javascript:var%20__hasjq=function(){console.log(%22test%22)};window.jQuery%26%26__hasjq();var%20s=document.createElement(%22script%22);s.src=%22//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js%22,s.onload=__hasjq,document.body.appendChild(s);']
+    ['var test;', 'javascript:!function($){var%20loadBookmarklet=function(){},hasJQuery=$%26%26$.fn%26%26parseFloat($.fn.jquery)%3E=1.7;if(hasJQuery)loadBookmarklet($);else{var%20s=document.createElement(%22script%22);s.src=%22//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js%22,s.onload=s.onreadystatechange=function(){var%20state=this.readyState;state%26%26%22loaded%22!==state%26%26%22complete%22!==state||loadBookmarklet(jQuery.noConflict())}}document.getElementsByTagName(%22head%22)[0].appendChild(s)}(window.jQuery);'],
+    ['console.log("test");', 'javascript:!function($){var%20loadBookmarklet=function(){console.log(%22test%22)},hasJQuery=$%26%26$.fn%26%26parseFloat($.fn.jquery)%3E=1.7;if(hasJQuery)loadBookmarklet($);else{var%20s=document.createElement(%22script%22);s.src=%22//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js%22,s.onload=s.onreadystatechange=function(){var%20state=this.readyState;state%26%26%22loaded%22!==state%26%26%22complete%22!==state||loadBookmarklet(jQuery.noConflict())}}document.getElementsByTagName(%22head%22)[0].appendChild(s)}(window.jQuery);']
   ];
 
   data.forEach(function (datum) {
