ba8121ec547e4cc6511f0b2ed469f457485f3e92
chriszarate <chris@zarate.org>
2014-12-03 23:12:32 -0500

    Update and simplify Angular app.

--- browser-tool.js
+++ browser-tool.js
@@ -1,44 +1,41 @@
 'use strict';
 
-// Load dependencies.
 var angular = require('angular');
 var bookmarkleter = require('./bookmarkleter');
 
-var App = angular.module('bookmarkleter', []);
+var app = angular.module('bookmarkleter', []);
 
-App.run(function($rootScope) {
+app.config(['$compileProvider', function ($compileProvider) {
+  $compileProvider.aHrefSanitizationWhitelist(/./);
+}]);
 
-  $rootScope.urlencode = true;
-  $rootScope.anonymize = true;
-  $rootScope.mangleVars = true;
+app.controller('appController', ['$scope', function ($scope) {
 
-  $rootScope.updateBookmarklet = function() {
+  $scope.options = {
+    urlencode: true,
+    anonymize: true,
+    mangleVars: true,
+    jQuery: false
+  };
 
-    var code = $rootScope.input;
+  $scope.error = null;
+
+  $scope.updateBookmarklet = function (options) {
+
+    var code = $scope.input;
 
     if(!code) return;
 
-    var bookmarkletOptions = {
-      urlencode: $rootScope.urlencode,
-      anonymize: $rootScope.anonymize,
-      jQuery: $rootScope.requiresJQuery,
-      mangleVars: $rootScope.mangleVars
-    };
-
     // Make a bookmarklet and show to user. Capture parse errors.
     try {
-      $rootScope.output = bookmarkleter(code, bookmarkletOptions);
-      $rootScope.error = null;
-    } catch(err) {
-      $rootScope.error = err.message;
-      $rootScope.output = null;
+      $scope.output = bookmarkleter(code, options);
+      $scope.error = null;
+    } catch (err) {
+      $scope.error = err.message;
+      $scope.output = null;
       return;
     }
 
   };
 
-});
-
-App.config(function($compileProvider) {
-  $compileProvider.aHrefSanitizationWhitelist(/./);
-});
+}]);
