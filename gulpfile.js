a86b202530e092f0c7e5113fb4b722f44cd3e118
chriszarate <chris@zarate.org>
2014-12-03 22:30:57 -0500

    Substitute gulp-ng-annotate for deprecated ngmin.

--- gulpfile.js
+++ gulpfile.js
@@ -20,10 +20,10 @@ gulp.task('clean', function () {
 // Browserify
 var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
-var ngmin = require('gulp-ngmin');
+var ngAnnotate = require('gulp-ng-annotate');
 gulp.task('browserify', function () {
   gulp.src('browser-tool.js')
-    .pipe(ngmin())
+    .pipe(ngAnnotate())
     .pipe(browserify())
     .pipe(uglify({preserveComments: 'some'}))
     .pipe(gulp.dest('./build'));
